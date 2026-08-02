8dfa753a2c4588fbc5a5c44ae409d0c2fea638ab
GetBoros <getboros@gmail.com>
2026-08-02 20:59:36 +0300

    feat(phys): Add wall and floor and tunnels, fast prototype

--- src/Scenes/Scene_Test.js
+++ src/Scenes/Scene_Test.js
@@ -1,14 +1,24 @@
 //------------------------------------------------------------------------------------------------------------
 import Phaser from 'phaser';
 //------------------------------------------------------------------------------------------------------------
+
+
+
+
+// AScene_Test
 class AScene_Test extends Phaser.Scene
-{// Test Scene. Inherits from Phaser.Scene.
+{// Test scene for Matter.js physics with tunnel walls, static floor container, and interactive spawning.
 
   constructor()
   {
-    super(
-    {
-      key: 'Scene_Test'
+    super({
+      key: 'Scene_Test',
+      physics: {
+        matter: {
+          gravity: { x: 0, y: 1 },
+          debug: true
+        }
+      }
     });
   }
 }
@@ -16,34 +26,106 @@ class AScene_Test extends Phaser.Scene
 
 
 
+
 // AScene_Test
 AScene_Test.prototype.create = function ()
 {
   let screen_center_x = 0;
-  let square_width = 0;
-  let square_height = 0;
-  let falling_square = null;
-  let static_ground = null;
+  let screen_center_y = 0;
+  let viewport_width = 0;
+  let viewport_height = 0;
+  let left_wall_vertices = null;
+  let right_wall_vertices = null;
 
   // 1.0. Determine viewport layout parameters
-  screen_center_x = this.cameras.main.width / 2;
-  square_width = 64;
-  square_height = 64;
+  viewport_width = this.cameras.main.width;
+  viewport_height = this.cameras.main.height;
+  screen_center_x = viewport_width / 2;
+  screen_center_y = viewport_height / 2;
 
-  // 2.0. Instantiate primitive dynamic actor (UE5 AActor + Rigid Body Component)
-  falling_square = this.add.rectangle(screen_center_x, 100, square_width, square_height, 0x00ff00);
-  this.physics.add.existing(falling_square, false);
+  // 2.0. Disable default browser context menu on right click
+  this.input.mouse.disableContextMenu();
 
-  // 2.1. Apply dynamic body parameters (Gravity, Bouncing, Viewport boundary limits)
-  falling_square.body.setCollideWorldBounds(true);
-  falling_square.body.setBounce(0.4, 0.4);
+  // 3.0. Define left curved tunnel wall vertices
+  left_wall_vertices = [
+    { x: screen_center_x - 120, y: 100 },
+    { x: screen_center_x - 20, y: 250 },
+    { x: screen_center_x - 120, y: 400 },
+    { x: screen_center_x - 20, y: 500 },
+    { x: screen_center_x - 20, y: 520 },
+    { x: screen_center_x - 140, y: 400 },
+    { x: screen_center_x - 40, y: 250 },
+    { x: screen_center_x - 140, y: 100 }
+  ];
 
-  // 3.0. Instantiate static platform actor (UE5 AActor + Static Collision Component)
-  static_ground = this.add.rectangle(screen_center_x, 500, 600, 32, 0x888888);
-  this.physics.add.existing(static_ground, true);
+  // 3.1. Register left wall static body
+  this.matter.add.fromVertices(screen_center_x - 80, 310, left_wall_vertices, {
+    isStatic: true
+  });
 
-  // 4.0. Register collision constraint inside physics subsystem
-  this.physics.add.collider(falling_square, static_ground);
+  // 4.0. Define right curved tunnel wall vertices
+  right_wall_vertices = [
+    { x: screen_center_x - 40, y: 100 },
+    { x: screen_center_x + 60, y: 250 },
+    { x: screen_center_x - 40, y: 400 },
+    { x: screen_center_x + 60, y: 500 },
+    { x: screen_center_x + 60, y: 520 },
+    { x: screen_center_x - 60, y: 400 },
+    { x: screen_center_x + 40, y: 250 },
+    { x: screen_center_x - 60, y: 100 }
+  ];
+
+  // 4.1. Register right wall static body
+  this.matter.add.fromVertices(screen_center_x, 310, right_wall_vertices, {
+    isStatic: true
+  });
+
+  // 5.0. Create bottom static floor platform (Catches objects leaving the tunnel)
+  this.matter.add.rectangle(screen_center_x, viewport_height - 20, viewport_width - 60, 32, {
+    isStatic: true,
+    restitution: 0.5
+  });
+
+  // 5.1. Create left container boundary wall
+  this.matter.add.rectangle(30, viewport_height - 100, 20, 160, {
+    isStatic: true
+  });
+
+  // 5.2. Create right container boundary wall
+  this.matter.add.rectangle(viewport_width - 30, viewport_height - 100, 20, 160, {
+    isStatic: true
+  });
+
+  // 6.0. Register mouse click input event listener
+  this.input.on('pointerdown', this.Handle_Pointer_Down, this);
+};
+//------------------------------------------------------------------------------------------------------------
+AScene_Test.prototype.Handle_Pointer_Down = function (pointer)
+{
+  let click_x = 0;
+  let click_y = 0;
+  let is_left_button = false;
+
+  // 1.0. Extract input coordinates and button state
+  click_x = pointer.x;
+  click_y = pointer.y;
+  is_left_button = (pointer.button === 0);
+
+  // 2.0. Spawn dynamic ball on Left Mouse Button or static obstacle block on Right Mouse Button
+  if (is_left_button)
+  {
+    this.matter.add.circle(click_x, click_y, 14, {
+      restitution: 0.8,
+      friction: 0.01
+    });
+  }
+  else
+  {
+    this.matter.add.rectangle(click_x, click_y, 80, 18, {
+      isStatic: true,
+      angle: Math.PI / 6
+    });
+  }
 };
 //------------------------------------------------------------------------------------------------------------
 AScene_Test.prototype.update = function (total_time, delta_time)
@@ -54,6 +136,6 @@ AScene_Test.prototype.update = function (total_time, delta_time)
 
 
 
-//------------------------------------------------------------------------------------------------------------
+
 export default AScene_Test;
 //------------------------------------------------------------------------------------------------------------
