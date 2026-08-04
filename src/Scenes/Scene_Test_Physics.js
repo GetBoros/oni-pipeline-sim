7b4e2fc68b0b95aefd0b1f52bf0975ac1477522b
GetBoros <getboros@gmail.com>
2026-08-04 21:39:53 +0300

    refact: add spawn bananas, deleted unused code

--- src/Scenes/Scene_Test_Physics.js
+++ src/Scenes/Scene_Test_Physics.js
@@ -47,14 +47,6 @@ AScene_Test_Physics.prototype.Add_Wall = function(wall_offset_x, wall_offset_y, 
   this.matter.add.rectangle(center_x, center_y, wall_width, wall_height, { isStatic: true } );
 };
 //------------------------------------------------------------------------------------------------------------
-AScene_Test_Physics.prototype.Add_Floor = function (floor_offset_x, floor_offset_y, floor_width, floor_height)
-{
-  let center_x = floor_offset_x + (floor_width / 2);
-  let center_y = floor_offset_y + (floor_height / 2);
-
-  this.matter.add.rectangle(center_x, center_y, floor_width, floor_height, { isStatic: true } );
-};
-//------------------------------------------------------------------------------------------------------------
 AScene_Test_Physics.prototype.Create_Level_Bounds = function()
 {
   let wall_offset_x = 9;
@@ -64,21 +56,17 @@ AScene_Test_Physics.prototype.Create_Level_Bounds = function()
   this.Add_Wall(wall_offset_x, wall_offset_y, wall_width, window.innerHeight);
   this.Add_Wall(window.innerWidth - wall_width - wall_offset_x, wall_offset_y, wall_width, window.innerHeight);
 
-  this.Add_Floor(9, window.innerHeight - 100, window.innerWidth - 18, 100);
+  this.Add_Wall(9, window.innerHeight - 100, window.innerWidth - 18, 100);
 };
 //------------------------------------------------------------------------------------------------------------
-AScene_Test_Physics.prototype.Create_Curve_Tunnel = function()
+AScene_Test_Physics.prototype.Spawn_Banan = function(world_x, world_y)
 {
-  let world_x = 0;
-  let world_y = 0;
-  let frame_index = 0;
-  let banana_vertices = null;
-  let banana_body = null;
-  let banana_sprite = null;
+  let frame_index;
+  let banana_vertices;
+  let banana_body;
+  let banana_sprite;
 
   // 1.0. Set target placement position on the viewport
-  world_x = window.innerWidth / 2;
-  world_y = 200;
   frame_index = 4;
 
   // 2.0. Pass raw GIMP 64x64 coordinates DIRECTLY from top-left (0, 0) - NO MANUAL SUBTRACTION!
@@ -107,73 +95,17 @@ AScene_Test_Physics.prototype.Create_Curve_Tunnel = function()
 AScene_Test_Physics.prototype.create = function ()
 {
   this.Create_Level_Bounds();
-  this.Create_Curve_Tunnel();
-
-  // // 4.0. Define right curved tunnel wall vertices
-  // right_wall_vertices = [
-  //   { x: screen_center_x - 40, y: 100 },
-  //   { x: screen_center_x + 60, y: 250 },
-  //   { x: screen_center_x - 40, y: 400 },
-  //   { x: screen_center_x + 60, y: 500 },
-  //   { x: screen_center_x + 60, y: 520 },
-  //   { x: screen_center_x - 60, y: 400 },
-  //   { x: screen_center_x + 40, y: 250 },
-  //   { x: screen_center_x - 60, y: 100 }
-  // ];
-
-  // // 4.1. Register right wall static body
-  // this.matter.add.fromVertices(screen_center_x, 310, right_wall_vertices, {
-  //   isStatic: true
-  // });
-
-  // // 5.0. Create bottom static floor platform (Catches objects leaving the tunnel)
-  // this.matter.add.rectangle(screen_center_x, viewport_height - 20, viewport_width - 60, 32, {
-  //   isStatic: true,
-  //   restitution: 0.5
-  // });
-
-  // // 5.1. Create left container boundary wall
-  // this.matter.add.rectangle(30, viewport_height - 100, 20, 160, {
-  //   isStatic: true
-  // });
-
-  // // 5.2. Create right container boundary wall
-  // this.matter.add.rectangle(viewport_width - 30, viewport_height - 100, 20, 160, {
-  //   isStatic: true
-  // });
+  this.Spawn_Banan(window.innerWidth / 2, 200);
 
   // 2.0. Disable default browser context menu on right click
   this.input.mouse.disableContextMenu();
+
   // 6.0. Register mouse click input event listener
-  this.input.on('pointerdown', this.Handle_Pointer_Down, this);
-};
-//------------------------------------------------------------------------------------------------------------
-AScene_Test_Physics.prototype.Handle_Pointer_Down = function (pointer)
-{
-  let click_x = 0;
-  let click_y = 0;
-  let is_left_button = false;
-
-  // 1.0. Extract input coordinates and button state
-  click_x = pointer.x;
-  click_y = pointer.y;
-  is_left_button = (pointer.button === 0);
-
-  // 2.0. Spawn dynamic ball on Left Mouse Button or static obstacle block on Right Mouse Button
-  if (is_left_button)
-  {
-    this.matter.add.circle(click_x, click_y, 14, {
-      restitution: 0.8,
-      friction: 0.01
-    });
-  }
-  else
-  {
-    this.matter.add.rectangle(click_x, click_y, 80, 18, {
-      isStatic: true,
-      angle: Math.PI / 6
-    });
-  }
+  this.input.on('pointerdown', (event_click) =>
+  { 
+    if (event_click.button === 0)  // if LKM - true, other false
+      this.Spawn_Banan(event_click.x, event_click.y)
+  } );
 };
 //------------------------------------------------------------------------------------------------------------
 AScene_Test_Physics.prototype.update = function (total_time, delta_time)
