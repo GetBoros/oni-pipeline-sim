e3cc7de8e14e45deaeab1630c50ad8bec137bf4e
GetBoros <getboros@gmail.com>
2026-08-19 21:49:31 +0300

    feat(save load): refactoring

---
+++ src/Core/Save_Manager.js
@@ -0,0 +1,84 @@
+//------------------------------------------------------------------------------------------------------------
+import Phaser from "phaser";
+//------------------------------------------------------------------------------------------------------------
+
+
+
+
+//------------------------------------------------------------------------------------------------------------
+class SSave_Data
+{
+    constructor()
+    {
+        this.Stage_Game = 'Qi Condensation';
+        this.Cultivation_Progress = 1.25;
+    }
+}
+//------------------------------------------------------------------------------------------------------------
+class ASave_Manager
+{
+    constructor(storage_key)
+    {
+        this.Storage_Key = storage_key;
+    }
+}
+//------------------------------------------------------------------------------------------------------------
+
+
+
+// ASave_Manager
+ASave_Manager.prototype.Save = function(save_data)
+{
+    let serializated_string = '';
+
+    try
+    {
+        serializated_string = JSON.stringify(save_data);
+        localStorage.setItem(this.Storage_Key, serializated_string);
+        console.log('Save done', json_str);
+    }
+    catch (error_exception)
+    {
+        console.log('Save failed', error_exception);
+    }
+};
+//------------------------------------------------------------------------------------------------------------
+ASave_Manager.prototype.Load = function (default_fallback_data)
+{
+    let parsed_data;
+    let raw_data_string;
+
+    raw_data_string = localStorage.getItem(this.Storage_Key);
+    if(raw_data_string === null)
+        return default_fallback_data;
+
+    try
+    {
+        parsed_data = JSON.parse(raw_data_string);
+        console.log('[SaveManager] State loaded successfully:', parsed_data);
+
+        return parsed_data;
+        
+    }
+    catch (error_exception)
+    {
+        console.error('[SaveManager] Failed to parse save data. Falling back to default:', error_exception);
+
+        return default_fallback_data;
+    }
+};
+//------------------------------------------------------------------------------------------------------------
+ASave_Manager.prototype.Clear = function ()
+{
+    localStorage.removeItem(this.Storage_Key);
+
+    console.log('[SaveManager] Save data cleared.');
+};
+//------------------------------------------------------------------------------------------------------------
+
+
+
+
+//------------------------------------------------------------------------------------------------------------
+export { SSave_Data, ASave_Manager };
+//------------------------------------------------------------------------------------------------------------
--- src/Scenes/Scene_Game.js
+++ src/Scenes/Scene_Game.js
@@ -3,6 +3,8 @@ import Phaser from 'phaser';
 
 import APortrait_Container from '../Containers/Portrait_Container';
 import ADebug_Container from '../Containers/Debug_Container';
+
+import { SSave_Data, ASave_Manager } from '../Core/Save_Manager';
 //------------------------------------------------------------------------------------------------------------
 
 
@@ -18,9 +20,9 @@ class AScene_Game extends Phaser.Scene
         this.Portrait_Container = null;
         this.Debug_Container = null;
 
-        this.Save_Storage_Key = 'cultivation_save_data_v1';
-        this.Player_Name = 'Andrey';
-        this.Cultivation_Progress = 0.0;
+        // Systems & State
+        this.Save_Manager = null;
+        this.Player_Data = null;
     }
 }
 //------------------------------------------------------------------------------------------------------------
@@ -34,15 +36,18 @@ AScene_Game.prototype.create = function()
     this.cameras.main.setBackgroundColor('rgba(0, 255, 0, 0.5)');  // !!! TEMP
     this.input.mouse.disableContextMenu();
 
-    this.Load_Game_State();
-    // this.Save_Game_State();
+    // 2.0. Initialize save subsystem and load state (BeginPlay equivalent)
+    this.Save_Manager = new ASave_Manager('cultivation_save_data_v1');
+    this.Player_Data = this.Save_Manager.Load(new SSave_Data() );
+
     this.Portrait_Container = new APortrait_Container(this, 100, 150);  // Create portrait at position
     this.Debug_Container = new ADebug_Container(this, 0, 0);
 
     this.Update_Layout(this.scale.width, this.scale.height);
     this.scale.on('resize', this.On_Window_Resize, this);
 
-    console.log('Player Cultivation Progress', this.Cultivation_Progress)
+    window.addEventListener('beforeunload', ()=> { this.Save_Manager.Save(); } );
+
 };
 //------------------------------------------------------------------------------------------------------------
 AScene_Game.prototype.On_Window_Resize = function(game_size)
@@ -66,36 +71,6 @@ AScene_Game.prototype.Update_Layout = function (width, height)
     {
         this.Debug_Container.Update_Layout(width - padding_x, padding_y);
         this.Debug_Container.Update_Text(width, height);
-    }
-};
-//------------------------------------------------------------------------------------------------------------
-AScene_Game.prototype.Save_Game_State = function()
-{
-    let container = null;
-    let json_str = null;
-
-    container = { stage_name: this.Player_Name, cultivation_progress: this.Cultivation_Progress };
-    json_str = JSON.stringify(container);
-
-    localStorage.setItem(this.Save_Storage_Key, json_str);
-
-    console.log('Save done', json_str);
-};
-//------------------------------------------------------------------------------------------------------------
-AScene_Game.prototype.Load_Game_State = function()
-{
-    let raw_data_str = localStorage.getItem(this.Save_Storage_Key);  // Get save game file find by name key
-
-    if(raw_data_str === null)
-    {
-        console.log('No save file found');
-    }
-    else
-    {
-        let parsed_data_json = JSON.parse(raw_data_str);
-
-        this.Player_Name = String(parsed_data_json.stage_name);
-        this.Cultivation_Progress = parseFloat(parsed_data_json.cultivation_progress);
     }
 };
 //------------------------------------------------------------------------------------------------------------
